e064e1ff52956ccb6971c70b8afb9ca77dcb50f4
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-08 23:22:21 -0500

    class6s: add test_factorial
    The factorial function isn't really working, though...

---
+++ hello-again/test_factorial.cxx
@@ -0,0 +1,12 @@
+
+#include "hello.h"
+
+#include <stdio.h>
+
+int main()
+{
+  printf("4! = %d\n", factorial(4));
+  printf("1! = %d\n", factorial(1));
+  printf("0! = %d\n", factorial(0));
+  return 0;
+}
