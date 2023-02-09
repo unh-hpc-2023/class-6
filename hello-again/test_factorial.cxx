64c6dc17d0c036367803aba609f43abea7783c53
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-08 23:30:28 -0500

    class6s: Use assert to automate tests

--- hello-again/test_factorial.cxx
+++ hello-again/test_factorial.cxx
@@ -1,12 +1,14 @@
 
 #include "hello.h"
 
-#include <stdio.h>
+#include <cassert>
 
 int main()
 {
-  printf("4! = %d\n", factorial(4));
-  printf("1! = %d\n", factorial(1));
-  printf("0! = %d\n", factorial(0));
+  assert(factorial(4) == 24);
+  assert(factorial(1) == 1);
+  assert(factorial(0) == 1);
+  // we don't have a good way to handle negative arguments, so we don't.
+  // Hence it doesn't make sense to test this case, either. FIXME
   return 0;
 }
